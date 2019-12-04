db9d5686394cf69027649ed5686a4051fc00000c
Andrew Mueller <iglooworks1505@gmail.com>
2019-12-04 13:02:41 -0600

    making progress on the loop with a time increment but have to leave so calling it quits for now

--- typeEffect.js
+++ typeEffect.js
@@ -4,50 +4,39 @@ var backSpace = false;
 var displayText = '';
 var index = 0;
 var i = 0;
-var typeAnim = function() {
-    displayText = nameArray[0].substring(0, displayText.length + 1);
-    console.log(displayText);
-};
-var Del = function (){
-    displayText = nameArray[0].substring(0, displayText.length -1);
-    console.log(displayText);
-};
-
-
-function type(){
-    if(backSpace == false){
-    typeAnim();
-    setTimeout(function() {
-        console.log(backSpace);
-      i++;
-      if (i < nameArray[0].length){
-        type();
-      }
-      else if (backSpace == false && index == 0){
-          backSpace = true;
-          console.log(backSpace);
-          type();
-      }
-    }, 500);
-  };
-
-  testing 
-  
-  if(backSpace == true){
-      Del();
-      setTimeout(function() {
-          i--;
-          if(i > 0){
-             type ();
-          };
-          if(i == 0){
-              console.log('increment in array here');
-          };
-      }, 150)
-  
-};
+var speed = 1500;
+if(backSpace == false){
+    var speed = 1400
 }
-type();
+if (backSpace !== false){
+    var speed = 200
+}
+
+
+function loopPause(){
+    setTimeout(function (){
+        if(backSpace !== true && i < nameArray[0].length){
+            i++;
+            displayText = nameArray[0].substring(0, displayText.length + 1);
+            console.log(displayText);
+            console.log(i);
+            loopPause()};
+            
+            if(i == nameArray[0].length){
+                setTimeout(() => { 
+
+                    // !!resume by adding delete script here 
+
+                    backSpace = true;
+                    console.log(backSpace)
+                }, 1500);
+            }
+        }, 400);
+    }; 
+
+loopPause();
+
+
 
 
 
@@ -58,6 +47,5 @@ type();
     //    console.log(displayText);
     
     
-
 
 
